969634ad183bd126c06365c8e18ba40fe21a4297
Golam Faruk Ovi <72310277+ov1faruk@users.noreply.github.com>
2023-03-15 12:44:44 +0600

    Update add_5_and_3_and_results.asm

--- Final_Lab1/add_5_and_3_and_results.asm
+++ Final_Lab1/add_5_and_3_and_results.asm
@@ -1,55 +1,19 @@
-.MODEL SMALL
-.STACK 100h
-.DATA
-a db ? ; here a=name , db=type , ?=unassigned value
-.CODE
+.model small
+.stack 100h
+.data
 
-MAIN PROC
-
-MOV CL,1
-add cl,30h
-MOV CH,2
-add ch,30h
+.code
+main proc
+    MOV BL,5
+    ADD BL,3
+    ADD BL,30h    
     
-mov ah,1
-int 21h 
-
-
-CMP al, 'a'
-JE show1
+    MOV AH,2 
+    MOV DL,BL
+    INT 21h
     
-jmp exit
-    
-CMP al, 'b'
-JE show2
      
-jmp exit    
-   
-   
-show1:
-
-
-    MOV AH, 2
-    MOV DL, CL
+    MOV AH,4ch
     INT 21h
-    
-show2:
-
-    MOV AH, 2
-    MOV DL, CH
-    INT 21h 
-    
-    
-exit:
-    MOV AH,4CH
-    INT 21h
-    
-
-
-   
-
-
-MAIN ENDP
-END MAIN
-
-ret
+    main endp
+end main
